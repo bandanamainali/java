bba2308ee392904e5dc06c88a5be106d3f361808
bandanamainali <75675904+bandanamainali@users.noreply.github.com>
2022-01-17 20:52:05 +0545

    assignment8

---
+++ assignment/assignment8/README.md
@@ -0,0 +1,3 @@
+## assigned date:16 jan
+## Deadline :17 jan
+## date of submission:17 jan
---
+++ assignment/assignment8/nepalmap.java
@@ -0,0 +1,69 @@
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Polygon;
+
+public class nepalmap extends JFrame
+{
+	public nepalmap()
+	{
+		super("drawing triangle and mapof nepal");
+		setSize(900, 900);
+		setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public void paint(Graphics g)
+	{
+        int x[]={200,300,400};
+        int y[]={70,45,70};
+       
+        Polygon p1=new Polygon(x,y,3);
+        g.setColor(Color.BLUE);
+        g.drawPolygon(p1);
+
+		int x1[]={500,500,900};
+        int y1[]={50,200,200};
+       
+        Polygon p2=new Polygon(x1,y1,3);
+        g.setColor(Color.GREEN);
+        g.drawPolygon(p2);
+		g.fillPolygon(p2);
+        Color crimson=new Color(153,0,0);
+        
+        int []x1points={30,30,30,30,30};
+        int []y1points={30,60,90,120,300};
+        Polygon p3=new Polygon(x1points,y1points,5);
+        g.setColor(Color.BLUE);
+        g.drawPolygon(p3);
+        
+      
+         int []x2points={30,120,30,120,30};
+         int []y2points={30,90,120,200,250};
+         Polygon p4=new Polygon(x2points,y2points,5);
+         g.drawPolygon(p4);
+         g.setColor(crimson);
+         g.fillPolygon(p4);
+         //star making
+         int []x3points={45,40,35,40,45,55,60,55};
+         int []y3points={150,185,185,195,230,195,185,185};
+         Polygon p5=new Polygon(x3points,y3points,8);
+         g.drawPolygon(p5);
+         g.setColor(Color.WHITE);
+         g.fillPolygon(p5);
+         //moon making
+         g.setColor(Color.WHITE);
+         g.fillArc(35,45,10,45,0,-180);
+
+
+
+        
+
+	}
+
+	public static void main(String[] args)
+	{
+		new nepalmap();
+	}
+
+}
